1f0bc3431c60cbc4a3b4a8a08fc0d40fd553b094
Маліцька Наталя <91392018+Natali27M@users.noreply.github.com>
2023-04-12 20:56:25 +0300

    create project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition';
+import {axiosService} from './services/axios.service';
+import {urls} from './configs/urls';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
+
+    if (!browserSupportsSpeechRecognition) {
+        return <span>Browser doesn't support speech recognition.</span>;
+    }
+    const getText = async() => {
+        console.log(transcript);
+        // async (data, {rejectWithValue}) => {
+        //     try {
+        //         return await exercisesProxyServices.checkExercises(data);
+        //     } catch (e) {
+        //         rejectWithValue(e);
+        //     }
+        // }
+
+        await axiosService.post(urls.image,{transcript}).then(value => value.data).then(value => console.log(value.data,'11111111111'))
+    }
+    return (
+        <div>
+            <p>Microphone: {listening ? 'on' : 'off'}</p>
+            <button onClick={SpeechRecognition.startListening}>Start</button>
+            <button onClick={SpeechRecognition.stopListening}>Stop</button>
+            <button onClick={resetTranscript}>Reset</button>
+            <p>{transcript}</p>
+            <button onClick={getText}>Send</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/configs/urls.js
@@ -0,0 +1,7 @@
+const baseURL = 'http://localhost:9090';
+
+export default baseURL;
+
+export const urls = {
+    image: '/image/image'
+};
